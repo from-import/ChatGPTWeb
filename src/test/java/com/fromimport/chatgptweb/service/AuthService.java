b0d25d5ebbcd8587ec014e0b4eeada28272517d5
import_urllib7 <132730866+from-import@users.noreply.github.com>
2024-08-17 03:16:29 +0800

    Delete src/test/java/com/fromimport directory

--- src/test/java/com/fromimport/chatgptweb/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.fromimport.chatgptweb.service;
-
-import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
-
-@Service
-public interface AuthService {
-    Mono<String> login(String username, String password);
-}
--- src/test/java/com/fromimport/chatgptweb/service/OpenAIService.java
+++
@@ -1,8 +0,0 @@
-package com.fromimport.chatgptweb.service;
-import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
-
-@Service
-public interface OpenAIService {
-    Mono<String> chatgpt(String message);
-}
